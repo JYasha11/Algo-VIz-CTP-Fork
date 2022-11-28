594de0cc31f027b155edf63daa75d330bdc77090
Jon <jonathanyashayev11@gmail.com>
2022-11-28 18:29:04 -0500

    Page additions

---
+++ client/src/pages/SignInPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class SignInPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+            Sign In Page Placeholder
+        </h1>
+      </div>
+    )
+  }
+}
