d5997fbcf8d5ef6145fc6be6fa64c961c552e520
eliantiudic <eliantiudic@gmail.com>
2022-12-14 15:26:31 -0500

    sorting vis styled further

--- client/src/pages/AboutUsPage.jsx
+++ client/src/pages/AboutUsPage.jsx
@@ -17,7 +17,7 @@ export const AboutUsPage = () => {
           <h2>Elian Tiudic</h2>
           <img src={elian} alt = "Elian's picture"/>
           <p class="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          Computer Science student at Booklyn College. Interested in Web Development, Data Science, and Web3.
           </p>
         </div>
 
--- client/src/pages/VisualizationsPage2.jsx
+++ client/src/pages/VisualizationsPage2.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import SortingVisualizer from "../components/sortingVisualizer/SortingVisualizer";
-import LinkedList from "../components/LinkedList";
+import SortingVisualizer from "../components/sortingVisualizer/SortingVisualizer"
 
 export const VisualizationsPage2 = () => {
-  return (
-    <div> 
-      <SortingVisualizer />
-    </div>>
-  )
+  return (<div> 
+    <SortingVisualizer />
+    </div>
+  );
 };
